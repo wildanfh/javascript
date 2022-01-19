99093e2365d9c83929ff12f912cee1e1e77d881b
wildanfh <wildanf2005@gmail.com>
2022-01-19 07:45:23 +0700

    belajar javascript dasar

---
+++ script.js
@@ -0,0 +1,116 @@
+// var x = 10;
+// if (x >= 10) {
+//   console.log("hello world");
+// }
+
+// var brea = 10;
+
+// console.log(brea);
+
+// var nama = "wildan",
+//   umur = 16,
+//   lulus = true;
+
+// if (lulus) {
+//   console.log(nama, umur);
+// }
+
+// var x = "hello world";
+// console.log("isi daro variabel x adalah " + x);
+
+// video 16 (popup box)
+
+// var klik = confirm("klik ok / cancel");
+
+// if (klik === true) {
+//   alert("anda benar");
+// } else {
+//   alert("anda salah");
+// }
+
+// alert("selamat datang");
+// var lagi = true;
+
+// while (lagi) {
+//   var nama = prompt("masukkan nama :");
+//   alert("halo " + nama + " !");
+
+//   lagi = confirm("lagi?");
+// }
+
+// alert("terima kasih");
+
+// video 17 -control flow-
+
+// var angka = Number;
+// var lagi = true;
+// while (lagi) {
+//   var input = prompt("masukkan angka :");
+//   if (angka === true) {
+//     alert("angka yang anda masukkan adalah : " + input);
+//   } else {
+//     alert(input + " bukan angka!!");
+//   }
+//   lagi = confirm("lagi?");
+// }
+
+// var x = "wildan";
+// alert("mulai");
+// for (var i = 0; i < 5; i++) {
+//   console.log(x);
+// }
+// alert("selesai");
+
+// var x = prompt("masukkan angka :");
+
+// if (x % 2 === 0) {
+//   alert(x + " merupakan bilangan Genap");
+// } else {
+//   alert(x + " merupakan bilangan Ganjil");
+// }
+
+// video 18 -pengulangan (while);
+
+// dihentikan oleh user
+// var lagi = true;
+// while (lagi) {
+//   console.log("halo");
+//   lagi = confirm("lagi?");
+// }
+
+// dihentikan program
+// var nilai = 1;
+// while (nilai <= 10) {
+//   console.log("Hello World");
+//   nilai++;
+// }
+
+// membuatnya berurutan
+// var nilai = 1;
+// while (nilai <= 1000) {
+//   console.log("Hello World " + nilai + "x");
+//   nilai++;
+// }
+
+// video 19 - tabel penelusuran -
+// latihan angkot 1
+// var jmlAngkot = 10;
+
+// for (var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++) {
+//   console.log("Angkot No. " + noAngkot + " beroperasi dengan baik.");
+// }
+
+// video 20 - pengulangan (for) -
+// latihan angkot 2
+var jmlAngkot = 10;
+var angkotBeroperasi = 6;
+var noAngkot = 1;
+
+while (noAngkot <= angkotBeroperasi) {
+  console.log("Angkot No. " + noAngkot + " beroperasi dengan baik.");
+  noAngkot++;
+}
+
+for (var noAngkot = angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++) {
+  console.log("Angkot No. " + noAngkot + " sedang tidak beroperasi.");
+}
